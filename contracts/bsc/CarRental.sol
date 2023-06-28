c09eeaf25ac09b4543681fe530201a3fe7dd4e15
Niko Febrianur <nikofebrianur@gmail.com>
2023-06-28 10:08:31 +0800

    first commit

---
+++ contracts/bsc/CarRental.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract CarRental {
+   // DATA
+
+   // Counter
+
+   // Owner
+
+   // totalPayments
+
+   // user struct
+
+   // car struct
+
+   // enum to indicate the status of the cara
+
+   // events
+
+   // user mapping
+
+   // car mapping
+
+   // constructor 
+
+   // MODIFIERS
+
+   // onylOwner
+
+   // FUNCTIONS
+
+   // Execute Functions
+
+   // setOwner #onlyOwner
+
+   // addUser #nonExisting
+
+   // addCar #onlyOwner #nonExistingCar
+
+   // editCarMetadata #onlyOwner #existingCar
+
+   // editCarStatus #onlyOwner #existingCar
+
+   // checkOut #existingUser #isCarAvailabe #userHasNotRentedACar #userHasNoDebt
+
+   // checkIn #existingUser #isCarAvailable #userHasRentedACar
+
+   // deposit #existingUser
+
+   // makePtyment #existingUser #existingDebt #sufficentBalance
+
+   // withdrawBalance #existingUser
+
+   // withdrawOwnerBalance #onlyOwner
+
+   // Query Functions
+
+   // getOwner
+
+   // isUser
+
+   // getUSer #existinguser
+
+   // getCar #existingCar
+
+   // getCarByStatus 
+
+   // calculateDebt
+
+   // getCurrentCount
+
+   // getContractBalance #onlyOwner
+
+   // getTotalPayment #onlyOwner
+}
